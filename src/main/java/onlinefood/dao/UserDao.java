24ce259e67e9a5bce78800570913263d8a03191d
prasannakumar6811 <99822777+prasannakumar6811@users.noreply.github.com>
2022-05-30 15:55:00 +0530

    initial commit

--- src/main/java/onlinefood/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package onlinefood.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import onlinefood.model.User;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-	public  User findByEmailId(String email);
-}
